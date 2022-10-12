507ab3550d278768fcfa2aa49d61534a118b23c7
Vishal Sainani <vishalsainani2000@gmail.com>
2022-10-13 01:19:46 +0500

    Deployed Version
    Deo

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-e0fbe/us-central1/api" // the api (cloud function url)
+    baseURL: "https://us-central1-clone-e0fbe.cloudfunctions.net/api"
+    // "http://localhost:5001/clone-e0fbe/us-central1/api" // the api (cloud function url)
 })
 
 
